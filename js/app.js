856b171939c4ff9e6be7b6b362b5fb9b6bf19817
hemme1m <markhemmert@gmail.com>
2017-08-10 07:52:57 -0400

    style changes, cleaned up script file

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
+(function () {
+
 var app = angular.module("awwApp", ["ui.bootstrap"]);
 
 app.controller('awwController', function($scope, $http) {
 
-	$scope.cuties = [];
-
     var url = "https://www.reddit.com/r/aww/.json?limit=15";
 
     $http.get(url).then(function(response) {
-		$scope.cuties = response.data.data.children;
-		console.log($scope.cuties)
+		$scope.cuties = response.data.data.children; //sends the top 15 reddit posts as objects to array
     });
 });
+
+})();
